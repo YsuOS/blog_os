cad58d1674bd017873a252fa8d29ba64ed769ad9
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2021-01-18 23:51:40 +0900

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
@@ -14,13 +17,8 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
