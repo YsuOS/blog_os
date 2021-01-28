7d3d9028b5f7b93e5bc82c088590091054dfd173
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2021-01-28 12:01:05 +0900

    Testing

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -118,19 +118,19 @@ impl fmt::Write for Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
-    //writer.write_string("Wörld!");
-}
+//pub fn print_something() {
+//    use core::fmt::Write;
+//    let mut writer = Writer {
+//        column_position: 0,
+//        color_code: ColorCode::new(Color::Yellow, Color::Black),
+//        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+//    };
+//
+//    writer.write_byte(b'H');
+//    writer.write_string("ello ");
+//    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+//    //writer.write_string("Wörld!");
+//}
 
 use lazy_static::lazy_static;
 use spin::Mutex;
@@ -162,3 +162,24 @@ pub fn _print(args: fmt::Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
 
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,39 +1,26 @@
-// disable standard library
 #![no_std]
-// disable all Rust-level entry points
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]  
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]  
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
+use blog_os::println;
+
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
