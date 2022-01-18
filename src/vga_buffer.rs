ea57d19f2e6f8b87b553cd472bf8b1304eab6d00
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2022-01-18 19:56:51 +0900

    add comment for fmt::Write implementation

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -114,6 +114,7 @@ impl Writer {
 
 }
 
+// To support `write!()` and `writeln!()` formatting macros
 impl fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
@@ -132,7 +133,6 @@ pub fn print_something() {
     writer.write_byte(b'H');
     writer.write_string("ello ");
     write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
-    //writer.write_string("Wörld!");
 }
 
 lazy_static! {
